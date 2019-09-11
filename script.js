97e9dc9082bed50ae7296be43917998dcc9473bc
Simon Onen <sonen8524@gmail.com>
2019-09-11 20:59:32 +0300

    Add kata for returning middle character

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function even_or_odd(number) {
 }
 
 even_or_odd(15);
+
+// You are going to be given a word. Your job is to return 
+// the middle character of the word. If the word's length 
+// is odd, return the middle character. If the word's length 
+// is even, return the middle 2 characters.
+
+function getMiddle(s) {
+    let stringLength = s.length;
+
+    if (stringLength === 1) {
+        return s[0];
+    } else {
+        if (!(stringLength % 2 === 0)) {
+            return s[(Math.floor(stringLength / 2))];
+        } else {
+            return `${s[Math.floor(stringLength / 2) - 1]}${s[Math.floor(stringLength / 2)]}`;
+        }
+    }
+}
+
+// test cases
+getMiddle('test');
+getMiddle('testing');
+getMiddle('middle');
+getMiddle('A');
